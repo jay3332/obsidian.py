d4f7d701685d822e3f6f5482ab7f14a25c438045
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-06-23 11:10:57 -0400

    Add `PointerBasedQueue`

--- obsidian/queue.py
+++ obsidian/queue.py
@@ -1,10 +1,18 @@
 from __future__ import annotations
 
+from enum import Enum
 from copy import copy
 from collections import deque
 from typing import Iterable, Iterator, Optional, Union
 
+
 from .track import Track, Playlist
+
+
+class LoopType(Enum):
+    NONE = 0
+    TRACK = 1
+    QUEUE = 2
 
 
 class QueueFull(Exception):
@@ -138,6 +146,9 @@ class Queue(Iterable[Track]):
     def get(self) -> Track:
         return self.pop()
 
+    def skip(self) -> Track:
+        return self.get()
+
     def insert(self, index: int, track: Union[Track, Playlist]) -> None:
         if isinstance(track, Playlist):
             raise TypeError(f'Playlists are not supported for insertion as of now.')
@@ -164,3 +175,120 @@ class Queue(Iterable[Track]):
     put = add
 
     __copy__ = copy
+
+
+class PointerBasedQueue(Queue):
+    """A certain type of queue that uses a pointer to point to a track in the queue, rather than popping tracks.
+
+    This allows for looping, therefore it is also implemented in this class.
+    """
+
+    def __init__(
+            self,
+            max_size: Optional[int],
+            *,
+            cls: type = deque,
+            default_loop_type: LoopType = LoopType.NONE
+    ) -> None:
+        super().__init__(max_size, cls=cls)
+
+        self.__loop_type: LoopType = default_loop_type
+        self.__current: Optional[int] = None
+
+    @property
+    def current(self) -> Track:
+        """
+        The current track the queue is pointing to.
+        """
+        return self[self.__current]
+
+    @current.setter
+    def current(self, new: int) -> None:
+        self.jump(new)
+
+    @property
+    def loop_type(self) -> LoopType:
+        """
+        The :class:`LoopType` of the queue.
+        """
+        return self.__loop_type
+
+    @loop_type.setter
+    def loop_type(self, new: LoopType) -> None:
+        self.set_loop_type(new)
+
+    def get(self) -> Optional[Track]:
+        """Retrieves the next track in the queue.
+
+        If the :class:`LoopType` is NONE and the queue has been exhausted,
+        this will return `None` instead.
+
+        If the :class:`LoopType` is TRACK, this will return the same track playing.
+        See :meth:`PointerBasedQueue.skip` to skip the current track.
+        """
+
+        if self.__current is None:
+            self.__current = 0
+
+        elif self.__loop_type is not LoopType.TRACK:
+            self.__current += 1
+
+        try:
+            return self.current
+        except IndexError:
+            if self.__loop_type is LoopType.QUEUE:
+                self.__current = 0
+                return self.current
+
+            return None
+
+    def skip(self) -> Optional[Track]:
+        """Skips the current track in the queue.
+
+        This will always retrieve a new track (or `None`).
+        See :meth:`PointerBasedQueue.get` if you aren't skipping the track.
+
+        This is different to :meth:`PointerBasedQueue.get` as it will
+        always skip the current track regardless of the LoopType.
+        """
+
+        if self.__current is None:
+            self.__current = 0
+        else:
+            self.__current += 1
+
+        try:
+            return self.current
+        except IndexError:
+            if self.__loop_type is LoopType.QUEUE:
+                self.__current = 0
+                return self.current
+
+            return None
+
+    def remove(self, track_or_index: Union[int, Track]) -> None:
+        before = self.current
+        super().remove(track_or_index)
+
+        if self.current != before:
+            self.__current -= 1
+
+    def insert(self, index: int, track: Union[Track, Playlist]) -> None:
+        super().insert(index, track)
+
+        if index <= self.__current:
+            self.__current += 1
+
+    def jump(self, index: int) -> Track:
+        self.__current = index
+        return self.current
+
+    def reset(self) -> None:
+        self.__current = None
+
+    def clear(self) -> None:
+        self.__current = None
+        super().clear()
+
+    def set_loop_type(self, new: LoopType) -> None:
+        self.__loop_type = new
