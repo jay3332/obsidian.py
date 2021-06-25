bec322dfda40889944e4d0c4ab78480db03a0128
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-25 13:25:21 -0700

    i think u need this thingy to render typehint

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'numpydoc',
     'sphinx.ext.extlinks',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
