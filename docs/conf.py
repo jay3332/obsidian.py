e359795d0a8d69f9f0a7719fc173bf90f057ed0f
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-25 14:37:50 -0700

    Show typehints in signature and as content infunction

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,7 @@ extensions = [
 
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
+autodoc_typehints = "both"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
