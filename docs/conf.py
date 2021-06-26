036c6b4a55b953af46941d98719d8d1d36e04d00
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-26 08:03:55 -0700

    Allow anchor links to work properly and change theme back

--- docs/conf.py
+++ docs/conf.py
@@ -74,20 +74,14 @@ pygments_style = "friendly"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nervproject'
+html_theme = 'karma_sphinx_theme'
 
-html_theme_options = {
-    "social": [
-    ],
-    "license": {
-        'type': 'MIT',
-        'url': 'https://raw.githubusercontent.com/jay3332/obsidian.py/master/LICENSE'
-    }
-}
+html_theme_options = {}
 
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["./_static"]
+
