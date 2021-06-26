27cdab186f231584b1a0d4a8593e9fa4e754a947
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-26 12:30:10 -0700

    Remove unsupported stuff

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,6 @@ extensions = [
 
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
-autodoc_typehints = "both"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
