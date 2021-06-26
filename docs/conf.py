67905c4091bd174a3e62cdcfb36c183e055e15d3
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-25 17:09:27 -0700

    Remove stuff we don't have

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ napoleon_numpy_docstring = True
 autodoc_typehints = "both"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 rst_prolog = """
 .. |coro| replace:: This function is a |coroutine_link|_.
@@ -80,4 +80,4 @@ needs_sphinx = "3.5.4"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
