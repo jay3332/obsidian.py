ae7b75bc11f1b46f144e681f248b234174157fb8
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-07-07 22:44:14 -0400

    Update node.py

--- obsidian/node.py
+++ obsidian/node.py
@@ -561,12 +561,13 @@ class Node(BaseNode, NodeListenerMixin):
         return self  # Relavant for NodeListenerMixin
 
     def dispatch(self, event: str, *args, **kwargs) -> None:
+        # Temporary solution that made in a PR
         for arg in args:
             if isinstance(arg, Player):
                 try:
                     getattr(arg, event)(*args, **kwargs)
                     return
-                except:
+                except AttributeError:
                     pass
                 
         self.bot.dispatch(event, *args, **kwargs)
